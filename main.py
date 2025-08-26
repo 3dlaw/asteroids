d924caf74779a6966b51061a8b0572a93047a5f2
3dlaw <dlawrence6293@gmail.com>
2025-08-25 23:52:08 -0400

    manage objects with groups

--- main.py
+++ main.py
@@ -4,18 +4,34 @@ from player import Player
 
 def main():
     pygame.init()
+    
     clock = pygame.time.Clock()
     dt = 0
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #print("containers:", getattr(Player, "containers", None))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0,0,0))
-        player.draw(screen)
-        player.update(dt)
+
+        updateable.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+   
         pygame.display.flip()
+
         dt = clock.tick(60)/1000
         
 
